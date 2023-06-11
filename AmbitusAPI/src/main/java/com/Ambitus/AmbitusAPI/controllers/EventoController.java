24a40c0c2e4233af1e441909c369a5f1ed0f9d1e
VitorHenri <vitorsantos.info@gmail.com>
2023-06-11 10:56:05 -0300

    Ajuste

--- AmbitusAPI/src/main/java/com/Ambitus/AmbitusAPI/controllers/EventoController.java
+++ AmbitusAPI/src/main/java/com/Ambitus/AmbitusAPI/controllers/EventoController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,17 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.Ambitus.AmbitusAPI.DTOs.DadosEvento;
-
-
-
 import com.Ambitus.AmbitusAPI.DTOs.DadosParticipantes;
 import com.Ambitus.AmbitusAPI.DTOs.MeusEventos;
 import com.Ambitus.AmbitusAPI.Security.BloqueioEdicao;
-
-
-import com.Ambitus.AmbitusAPI.DTOs.DadosParticipantes;
-import com.Ambitus.AmbitusAPI.DTOs.MeusEventos;
-
 import com.Ambitus.AmbitusAPI.entities.Evento;
 import com.Ambitus.AmbitusAPI.entities.Usuario;
 import com.Ambitus.AmbitusAPI.repositories.EventoRepository;
