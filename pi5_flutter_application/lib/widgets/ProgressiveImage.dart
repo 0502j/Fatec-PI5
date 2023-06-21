75409f72e7f0f2a102c355105f4513c606a0108d
jamisousa <jamiledsousa@gmail.com>
2023-06-21 09:11:16 -0300

    Alterando configuração do widget progressiveimage

--- pi5_flutter_application/lib/widgets/ProgressiveImage.dart
+++ pi5_flutter_application/lib/widgets/ProgressiveImage.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:progressive_image/progressive_image.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 /*
 
   Widget usado SOMENTE em imagens que não precisam de requisição com back-end, 
   como exemplo página de introdução após registro do usuário, trazendo efeito
-  de progressão de carregamento.
+  de progresso de carregamento.
 
 
  */
@@ -31,26 +30,28 @@ class ProgressiveImageWidget extends StatelessWidget {
     if (isOval == true) {
       return ClipOval(
         child: ProgressiveImage(
-          placeholder: const AssetImage('assets/images/loading.gif'),
-          thumbnail: imgPath != AssetImage('assets/images/loading.gif')
+          placeholder:
+              const AssetImage('assets/images/blurred-placeholder.jpg'),
+          thumbnail: imgPath != AssetImage('blurred-placeholder.jpg')
               ? AssetImage(imgPath!)
-              : AssetImage('assets/images/loading.gif'),
+              : AssetImage('assets/images/blurred-placeholder.jpg'),
           image: imgPath != null
               ? AssetImage(imgPath!)
-              : AssetImage('assets/images/loading.gif'),
+              : AssetImage('assets/images/blurred-placeholder.jpg'),
           height: heightValue ?? 100,
           width: widthValue ?? 100,
         ),
       );
     }
+
     return ProgressiveImage(
-      placeholder: AssetImage('assets/images/loading.gif'),
+      placeholder: AssetImage('assets/images/blurred-placeholder.jpg'),
       thumbnail: imgPath != null
           ? AssetImage(imgPath!)
-          : AssetImage('assets/images/loading.gif'),
+          : AssetImage('assets/images/blurred-placeholder.jpg'),
       image: imgPath != null
           ? AssetImage(imgPath!)
-          : AssetImage('assets/images/loading.gif'),
+          : AssetImage('assets/images/blurred-placeholder.jpg'),
       height: heightValue ?? 100,
       width: widthValue ?? 100,
     );
